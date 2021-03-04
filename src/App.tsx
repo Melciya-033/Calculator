34e0068ad17024fdd88076f64d8010b24c714950
Melciya-033 <melciyadhanasingh033@gmail.com>
2021-03-03 13:31:25 -1100

    calculator app ts files added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState,useEffect,useRef} from 'react';
+import { setConstantValue } from 'typescript';
+
 import './App.css';
 
 function App() {
+  const[result,setResult]=useState("");
+  const inputRef=useRef(null);
+  useEffect(()=>inputRef.current.focus());
+  
+  
+  function handleClick(e){
+    setResult(result+e.target.name);
+  }
+  function click(e){
+    setResult(result+e.target.id);
+  }
+  
+  function calculate(){
+    try{
+   setResult(eval(result).toString());
+  }
+    catch(error){
+    setResult("Error");
+
+   }
+
+  }
+  function clear(){
+    setResult("");
+  }
+  function convert(){
+   if(eval(result)>=0){
+      setResult("-"+ eval(result))
+    
+     
+    }
+    else{
+      setResult("+"+ eval(result))
+    }
+    
+  }
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <input type="text" value={result} ref={inputRef}/>
+        
+    
+    <div className="keypad">
+      <button  className="Button" name="AC" onClick={clear}>AC</button>
+      <button className="Button" name="+/-" onClick={convert}>+/-</button>
+      <button className="Button" name="%"onClick={handleClick} >%</button>
+      <button  className="Button1" name="/"onClick={handleClick}>/</button>
+      <button className="Button" name="7"onClick={handleClick}>7</button>
+      <button className="Button" name="8"onClick={handleClick}>8</button>
+      <button  className="Button"name="9"onClick={handleClick}>9</button>
+      <button  className="Button1" name="*"onClick={handleClick}>*</button>
+      <button  className="Button"name="4"onClick={handleClick}>4</button>
+      <button className="Button"name="5"onClick={handleClick}>5</button>
+      <button className="Button" name="6"onClick={handleClick}>6</button>
+      <button  className="Button1" name="-"onClick={handleClick}>-</button>
+      <button className="Button" name="1"onClick={handleClick}>1</button>
+      <button  className="Button"name="2"onClick={handleClick}>2</button>
+      <button className="Button" name="3"onClick={handleClick}>3</button>
+      <button   className="Button1"name="+"onClick={handleClick}>+</button>
+      <button className="Button" id="long" name="0"onClick={handleClick} >0</button>
+      <button className="Button" name="." onClick={handleClick}>.</button>
+      <button className="Button1" onClick={calculate}>=</button>
+      
+     
+
     </div>
+</div>
   );
 }
 
